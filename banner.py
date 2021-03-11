9e7d8eddb0bfe1661936b200184c54a776c43b29
H4CK6R SPY <71980380+vjkingler@users.noreply.github.com>
2021-03-11 18:54:27 +0530

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,30 @@
+#!/user/bin/python
+
+
+#modules required
+import os
+import time
+import sys
+import random
+os.system("clear")
+
+#colours used
+red = '\033[31m'
+yellow = '\033[93m'
+lgreen = '\033[92m'
+clear = '\033[0m'
+bold = '\033[01m'
+cyan = '\033[96m'
+
+#banner of script
+print (red+"""
+██╗  ██╗     ██████╗     ██╗  ██╗           
+██║  ██║    ██╔═████╗    ██║  ██║             ╗
+███████║    ██║██╔██║    ███████║        
+╚════██║    ████╔╝██║    ╚════██║       
+     ██║    ╚██████╔╝         ██║          
+     ╚═╝     ╚═════╝          ╚═╝               
+"""+red)
+
+print (lgreen+bold+"      <==========================[[ coded by H4CK6R SPY ]]==============================> \n"+clear)
+print (yellow+bold+"   <------------------------[[[   instagram @h4ck6r_spy   ]]]--------------------------------> \n"+clear)
