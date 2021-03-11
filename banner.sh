f99f177db8263c9f7898b9fba2635849929505f7
H4CK6R SPY <71980380+vjkingler@users.noreply.github.com>
2021-03-11 18:32:26 +0530

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#clolors
+white='\e[1;37m'
+green='\e[0;32m'
+blue='\e[1;34m'
+red='\e[1;31m'
+yellow='\e[1;33m' 
+echo ""
+echo""
+banner() {
+ echo -e $'\e[1;31m     ██████╗  ██████╗ ███████╗   
+ echo -e $'\e[1;31m     ██╔══██╗██╔═══██╗██╔════╝ 
+ echo -e $'\e[1;31m     ██║  ██║██║   ██║███████╗ 
+ echo -e $'\e[1;31m     ██║  ██║██║   ██║╚════██║   
+ echo -e $'\e[1;31m     ██████╔╝╚██████╔╝███████║   
+ echo -e $'\e[1;31m     ╚═════╝ ╚═════╝ ╚══════╝  
+   
+  echo""    
+  echo -e $'\e[1;32m DOS ATTACKING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [V.0.1] \e[0m'                                       
+  echo ""
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Coded By SPY \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
+   echo ""
+      }
+      banner
