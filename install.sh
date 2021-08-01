485e25e7bf9054a3cecaae0701ac62bb88715abd
H4CK6R SPY <71980380+vjkingler@users.noreply.github.com>
2021-08-01 14:28:42 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt update
+apt upgrade
+apt install python -y
+apt install python2 -y
+apt install figlet -y
+clear
+chmod+x *
+echo "[+] this tool for only educational purpose only"
+read -p "[+] you want continue: "y/n 
+python2 dos.py
+
